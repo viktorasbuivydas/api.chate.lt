9fd342252d48f3cecdfddbfa35e4ad84f225e69e
viktorasbuivydas <viktoras162@gmail.com>
2022-12-04 20:41:40 +0200

    update

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -36,12 +36,10 @@ return [
             'secret' => env('PUSHER_APP_SECRET'),
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
-                'cluster' => env('PUSHER_APP_CLUSTER'),
-                'encrypted' => true,
-                'host' => '127.0.0.1',
-                'port' => 6001,
+                'host'   => '127.0.0.1',
+                'port'   => 6001,
                 'scheme' => 'http',
-                'useTLS' => false,
+                'useTLS' => false, // this is no error, we are talking without SSL to the WebSocket server from Laravel but your end-users will connect with SSL
             ],
         ],
 
